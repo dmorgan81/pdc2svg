0a12afc7c7767e391238be492b902d8e7c5df975
David Morgan <dmorgan81@gmail.com>
2018-01-18 11:37:23 -0500

    Initial commit

---
+++ config.mk
@@ -0,0 +1,21 @@
+NAME = pdc-reader
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+# includes and libs
+INCS = -I. -I/usr/include
+LIBS = -L/usr/lib -lc
+
+# flags
+CFLAGS = -g -std=c99 -pedantic -Wall -O0 -D_POSIX_SOURCE ${INCS} ${CPPFLAGS}
+#CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS = -g ${LIBS}
+#LDFLAGS = -s ${LIBS}
+
+# compiler and linker
+CC = cc
+
---
+++ pdc-reader.c
@@ -0,0 +1,170 @@
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+
+static const uint8_t LEN_MAGIC = 4;
+
+struct point {
+    int16_t x;
+    int16_t y;
+};
+
+struct viewbox {
+    int16_t w;
+    int16_t h;
+};
+
+struct __attribute__((packed)) pdc {
+    uint8_t type;
+    uint8_t flags;
+    int8_t stroke_color;
+    uint8_t stroke_width;
+    int8_t fill_color;
+    union {
+        uint8_t path_type;
+        uint16_t radius;
+    } path_type_or_radius;
+    uint16_t num_points;
+    struct point *points;
+};
+
+struct __attribute__((packed)) pdc_list {
+    uint16_t num_commands;
+    struct pdc *commands;
+};
+
+struct __attribute__((packed)) pdc_frame {
+    uint16_t duration;
+    struct pdc_list command_list;
+};
+
+struct __attribute__((packed)) pdc_image {
+    uint8_t version;
+    uint8_t reserved;
+    struct viewbox viewbox;
+    struct pdc_list command_list;
+};
+
+struct __attribute__((packed)) pdc_seq {
+    uint8_t version;
+    uint8_t reserved;
+    struct viewbox viewbox;
+    int16_t play_count;
+    int16_t frame_count;
+    struct pdc_frame *frames;
+};
+
+static inline void prv_read_point(struct point *point, char *bytes) {
+    memcpy(&point->x, &bytes[0], sizeof(int16_t));
+    memcpy(&point->y, &bytes[2], sizeof(int16_t));
+}
+
+static void prv_read_pdc(struct pdc *pdc, char *bytes) {
+    memcpy(pdc, &bytes[0], 9);
+    pdc->points = malloc(sizeof(struct point) * pdc->num_points);
+    for (size_t i = 0; i < pdc->num_points; i++) {
+        prv_read_point(&pdc->points[i], bytes + 9 + (4 * i));
+    }
+}
+
+static void prv_read_pdc_list(struct pdc_list *list, char *bytes) {
+    memcpy(&list->num_commands, &bytes[0], sizeof(uint16_t));
+    list->commands = malloc(sizeof(struct pdc) * list->num_commands);
+    size_t offset = 2;
+    for (size_t i = 0; i < list->num_commands; i++) {
+        prv_read_pdc(&list->commands[i], bytes + offset);
+        offset += 9 + (sizeof(struct point) * list->commands[i].num_points);
+    }
+}
+
+static inline void prv_read_viewbox(struct viewbox *viewbox, char *bytes) {
+    memcpy(&viewbox->w, &bytes[0], sizeof(int16_t));
+    memcpy(&viewbox->h, &bytes[2], sizeof(int16_t));
+}
+
+static void prv_read_image(FILE *fp, char *name) {
+    uint32_t size;
+    if (fread(&size, sizeof(uint32_t), 1, fp) != 1) {
+        fprintf(stderr, "Error reading file: %s\n", name);
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    void *ret = mmap(0, size, PROT_READ, MAP_PRIVATE, fileno(fp), 0);
+    if (ret == MAP_FAILED) {
+        fprintf(stderr, "mmap: %s\n", strerror(errno));
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    char *bytes = (char*) ret;
+
+    struct pdc_image image;
+    image.version = (uint8_t) bytes[8];
+    prv_read_viewbox(&image.viewbox, bytes + 10);
+
+    printf("%d %d %d\n", image.version, image.viewbox.w, image.viewbox.h);
+
+    prv_read_pdc_list(&image.command_list, bytes + 14);
+    printf("%d\n", image.command_list.num_commands);
+
+    for (size_t i = 0; i < image.command_list.num_commands; i++) {
+        struct pdc command = image.command_list.commands[i];
+        printf("\t%d %d\n", command.type, command.num_points);
+        for (size_t j = 0; j < command.num_points; j++) {
+            printf("\t\t%d %d\n", command.points[j].x, command.points[j].y);
+        }
+
+        free(command.points);
+    }
+    free(image.command_list.commands);
+
+    if (munmap(ret, size) == -1) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void prv_read_seq(FILE *fp, char *name) {}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "usage: pdc-reader PDC\n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+    
+    if (!fp) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    char magic[LEN_MAGIC + 1];
+    if (fread(magic, sizeof(char), LEN_MAGIC, fp) != LEN_MAGIC) {
+        fprintf(stderr, "Error reading file: %s\n", argv[1]);
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+    magic[LEN_MAGIC] = '\0';
+
+    if (strncmp(magic, "PDCI", LEN_MAGIC) == 0) {
+        prv_read_image(fp, argv[1]);
+    } else if (strncmp(magic, "PDCS", LEN_MAGIC) == 0) {
+        prv_read_seq(fp, argv[1]);
+    } else {
+        fprintf(stderr, "Not a PDC file: %s\n", argv[1]);
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(fp);
+    fp = NULL;
+
+    exit(EXIT_SUCCESS);
+}
